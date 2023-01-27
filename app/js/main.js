b1650ab8ff69a467779f44ef6d87a32eff97a398
Komaridthe <kto_prik@list.ru>
2023-01-27 14:40:53 +0300

    2.0.1 (styles to slider)

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,20 @@ $(function () {
   $(".top__slider").slick({
     arrows: false,
     dots: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     responsive: [
-      {
-        breakpoint: 840,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 601,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
+      // {
+      //   breakpoint: 840,
+      //   settings: {
+      //     slidesToShow: 1,
+      //   },
+      // },
+      // {
+      //   breakpoint: 601,
+      //   settings: {
+      //     slidesToShow: 1,
+      //   },
+      // },
     ],
   });
 });
